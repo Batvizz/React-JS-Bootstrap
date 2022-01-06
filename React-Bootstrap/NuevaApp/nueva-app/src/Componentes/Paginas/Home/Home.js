51a68a40a4a4fd4e5a8e5cb58b0ca6c4d9846fc3
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2022-01-06 19:35:20 -0300

    1

--- React-Bootstrap/NuevaApp/nueva-app/src/Componentes/Paginas/Home/Home.js
+++ React-Bootstrap/NuevaApp/nueva-app/src/Componentes/Paginas/Home/Home.js
@@ -1,27 +1,70 @@
 import { Button, Navbar, Nav, Item } from 'react-bootstrap'
+import React, { useState } from 'react';
+import {
+    MDBContainer,
+    MDBNavbar,
+    MDBNavbarBrand,
+    MDBNavbarToggler,
+    MDBIcon,
+    MDBNavbarNav,
+    MDBNavbarItem,
+    MDBNavbarLink,
+    MDBBtn,
+    MDBDropdown,
+    MDBDropdownToggle,
+    MDBDropdownMenu,
+    MDBDropdownItem,
+    MDBDropdownLink,
+    MDBCollapse
+} from 'mdb-react-ui-kit';
 
-const Home = () => (
 
-    <Nav
-        activeKey="/home"
-        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-    >
-        <Nav.Item>
-            <Nav.Link href="/home">Active</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-            <Nav.Link eventKey="link-1">Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-            <Nav.Link eventKey="link-2">Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-            <Nav.Link eventKey="disabled" disabled>
-                Disabled
-            </Nav.Link>
-        </Nav.Item>
-    </Nav>
+export default function Home() {
 
-)
+    const [showBasic, setShowBasic] = useState(false);
 
-export default Home;
+    return (
+
+        <MDBNavbar expand='lg' light bgColor='dark'>
+            <MDBContainer fluid>
+                <MDBNavbarBrand href='#'> Inicio </MDBNavbarBrand>
+
+                <MDBNavbarToggler aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation' onClick={() => setShowBasic(!showBasic)}>
+
+                    <MDBIcon icon='bars' fas />
+
+                </MDBNavbarToggler>
+
+                <MDBCollapse navbar show={showBasic}>
+                    <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
+                        <MDBNavbarItem>
+                            <MDBNavbarLink aria-current='page' href='#'>
+                                Documentacion
+                            </MDBNavbarLink>
+                        </MDBNavbarItem>
+
+                        <MDBNavbarItem>
+                            <MDBNavbarLink href='#'>Link</MDBNavbarLink>
+                        </MDBNavbarItem>
+
+                        <MDBNavbarItem>
+
+                        </MDBNavbarItem>
+
+                        <MDBNavbarItem>
+                            <MDBNavbarLink disabled href='#' tabIndex={-1} aria-disabled='true'>
+                                Disabled
+                            </MDBNavbarLink>
+                        </MDBNavbarItem>
+                    </MDBNavbarNav>
+
+                    <form className='d-flex input-group w-auto'>
+                        <input type='search' className='form-control' placeholder='Type query' aria-label='Search' />
+                        <MDBBtn color='primary'>Search</MDBBtn>
+                    </form>
+                </MDBCollapse>
+            </MDBContainer>
+        </MDBNavbar>
+    );
+}
+
