979ef70d1c63cbdac646b6856cd0c466d9d75322
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2022-01-23 23:21:31 -0300

    1

--- React-Bootstrap/NuevaApp/nueva-app/src/Componentes/Paginas/Home/Home.js
+++ React-Bootstrap/NuevaApp/nueva-app/src/Componentes/Paginas/Home/Home.js
@@ -24,47 +24,47 @@ export default function Home() {
     const [showBasic, setShowBasic] = useState(false);
 
     return (
+        
+            <MDBNavbar expand='lg' light bgColor='light'>
+                <MDBContainer fluid>
+                    <MDBNavbarBrand href='#'> Inicio </MDBNavbarBrand>
 
-        <MDBNavbar expand='lg' light bgColor='light'>
-            <MDBContainer fluid>
-                <MDBNavbarBrand href='#'> Inicio </MDBNavbarBrand>
+                    <MDBNavbarToggler aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation' onClick={() => setShowBasic(!showBasic)}>
 
-                <MDBNavbarToggler aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation' onClick={() => setShowBasic(!showBasic)}>
+                        <MDBIcon icon='bars' fas />
 
-                    <MDBIcon icon='bars' fas />
+                    </MDBNavbarToggler>
 
-                </MDBNavbarToggler>
+                    <MDBCollapse navbar show={showBasic}>
+                        <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
+                            <MDBNavbarItem>
+                                <MDBNavbarLink aria-current='page' href='#'>
+                                    Documentacion
+                                </MDBNavbarLink>
+                            </MDBNavbarItem>
 
-                <MDBCollapse navbar show={showBasic}>
-                    <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
-                        <MDBNavbarItem>
-                            <MDBNavbarLink aria-current='page' href='#'>
-                                Documentacion
-                            </MDBNavbarLink>
-                        </MDBNavbarItem>
+                            <MDBNavbarItem>
+                                <MDBNavbarLink href='#'>Link</MDBNavbarLink>
+                            </MDBNavbarItem>
 
-                        <MDBNavbarItem>
-                            <MDBNavbarLink href='#'>Link</MDBNavbarLink>
-                        </MDBNavbarItem>
+                            <MDBNavbarItem>
 
-                        <MDBNavbarItem>
+                            </MDBNavbarItem>
 
-                        </MDBNavbarItem>
+                            <MDBNavbarItem>
+                                <MDBNavbarLink disabled href='#' tabIndex={-1} aria-disabled='true'>
+                                    Disabled
+                                </MDBNavbarLink>
+                            </MDBNavbarItem>
+                        </MDBNavbarNav>
 
-                        <MDBNavbarItem>
-                            <MDBNavbarLink disabled href='#' tabIndex={-1} aria-disabled='true'>
-                                Disabled
-                            </MDBNavbarLink>
-                        </MDBNavbarItem>
-                    </MDBNavbarNav>
-
-                    <form className='d-flex input-group w-auto'>
-                        <input type='search' className='form-control' placeholder='Type query' aria-label='Search' />
-                        <MDBBtn color='primary'>Search</MDBBtn>
-                    </form>
-                </MDBCollapse>
-            </MDBContainer>
-        </MDBNavbar>
+                        <form className='d-flex input-group w-auto'>
+                            <input type='search' className='form-control' placeholder='Type query' aria-label='Search' />
+                            <MDBBtn color='primary'>Search</MDBBtn>
+                        </form>
+                    </MDBCollapse>
+                </MDBContainer>
+            </MDBNavbar>
     );
 }
 
